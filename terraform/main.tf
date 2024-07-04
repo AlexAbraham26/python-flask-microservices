53a32c873fae98160299f4c11f29690d27f907e1
Alex <abraha7@uw.edu>
2024-07-04 09:05:46 -0400

    Removed duplicate code and fixed syntax error

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,3 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
-
-output "acr_admin_password" {
-  value = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
-
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
