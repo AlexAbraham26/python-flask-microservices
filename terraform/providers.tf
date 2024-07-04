20b1e9f014ae2c4ed88af7029aecd8386d0cd479
Alex <abraha7@uw.edu>
2024-07-04 19:02:27 -0400

    Changed required tf version to >= 1.0.0

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,8 +13,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.57.0" 
+      version = "~> 3.0.0" 
     }
   }
-  required_version = ">= 1.5.7"
+  required_version = ">= 1.0.0"
 }
