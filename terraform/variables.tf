99b50e624ec9b65ceff8bb0e57a5adbd84513df5
Alex <abraha7@uw.edu>
2024-07-05 02:58:44 -0400

    Edited tf files to run azure resources locally

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,22 @@
-# variable "client_id" {
-#   description = "Client ID for the Azure Service Principal"
-#   type        = string
-# }
+variable "client_id" {
+  description = "Client ID for the Azure Service Principal"
+  type        = string
+}
 
-# variable "client_secret" {
-#   description = "Client Secret for the Azure Service Principal"
-#   type        = string
-# }
+variable "client_secret" {
+  description = "Client Secret for the Azure Service Principal"
+  type        = string
+}
 
-# variable "subscription_id" {
-#   description = "Subscription ID for the Azure subscription"
-#   type        = string
-# }
+variable "subscription_id" {
+  description = "Subscription ID for the Azure subscription"
+  type        = string
+}
 
-# variable "tenant_id" {
-#   description = "Tenant ID for the Azure subscription"
-#   type        = string
-# }
+variable "tenant_id" {
+  description = "Tenant ID for the Azure subscription"
+  type        = string
+}
 
 variable "location" {
   description = "The Azure region where resources will be deployed."
